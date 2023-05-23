f4fc09752343317a737dcae48894fd8df18a8de1
paulatanu1 <atanupaul76@gmail.com>
2023-05-24 00:23:59 +0530

    API service intregation

--- frontend/src/app/common-service/api.service.ts
+++ frontend/src/app/common-service/api.service.ts
@@ -1,9 +1,90 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map, timeout, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs'
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient,) { }
+
+  queryParams(source: any) {
+    var array = [];
+    for (var key in source) {
+      array.push(encodeURIComponent(key) + "=" + encodeURIComponent(source[key]));
+    }
+    return array.join("&");
+  }
+
+  /**
+    @Date : 23-05-2023
+    @Developer : Atanu Paul
+    @param url : api url to call like '/regisatration ,/login'
+    @param method : method to call like post or put or get or delete
+   */
+  ApiCallWithLocalization(data: any, url: string, method: string, headertoken: any = undefined) {
+    return this.ApiCall(data, url, method, headertoken);
+  }
+
+  ApiCall(data: any, url: string, method: string, headertoken: any = undefined, IsUrlrawformat: boolean = false) {
+    let paths: string[] = location.pathname.split('/').splice(1, 1);
+    
+
+    if (IsUrlrawformat == false) {
+      url = environment.API_URL + '/' + environment.API_VERSION + url;
+    }
+
+    let httpHeaderValue = new HttpHeaders();
+
+    if (headertoken == undefined) {
+      httpHeaderValue = httpHeaderValue
+    }
+    else {
+      httpHeaderValue = httpHeaderValue
+        .set('Authorization', 'Bearer ' + headertoken)
+    }
+
+    if (method == 'post') {
+      return this.http.post(url, data, { headers: httpHeaderValue, observe: 'response' })
+        .pipe(
+          timeout(environment.timeout),
+          catchError((e, c) => { return throwError(e) }),
+          map((response: any) => {
+            var responseobj = JSON.parse(JSON.stringify(response.body));
+            responseobj.status = response.status;
+            return responseobj;
+          })
+        )
+
+    }
+    else if (method == 'get') {
+      return this.http.get(url, { headers: httpHeaderValue, observe: 'response' })
+        .pipe(
+          timeout(environment.timeout),
+          catchError((e, c) => { return throwError(e) }),
+          map((response: any) => {
+            var responseobj = JSON.parse(JSON.stringify(response.body));
+            responseobj.status = response.status;
+            return responseobj;
+          })
+        )
+
+    }
+    else {
+      return this.http.post(url, data, { headers: httpHeaderValue, observe: 'response' })
+        .pipe(
+          timeout(environment.timeout),
+          catchError((e, c) => { return throwError(e) }),
+          map((response: any) => {
+            var responseobj = JSON.parse(JSON.stringify(response.body));
+            responseobj.status = response.status;
+            return responseobj;
+          })
+        )
+
+    }
+  }
+
 }
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  timeout:2500,
+  API_URL:'http://localhost:5500/',
+  API_VERSION:'api'
 };
 
 /*
