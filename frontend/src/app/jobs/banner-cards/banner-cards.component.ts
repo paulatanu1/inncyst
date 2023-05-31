39c1a267c28edeafacbd9f081a6bda083c50f611
Sohini Sengupta <sohini.sengupta@massoftind.com>
2023-06-01 01:44:31 +0530

    job page implementation done

---
+++ frontend/src/app/jobs/banner-cards/banner-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-cards',
+  templateUrl: './banner-cards.component.html',
+  styleUrls: ['./banner-cards.component.scss']
+})
+export class BannerCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
