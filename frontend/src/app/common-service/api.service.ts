3566e819b1c52676c72da30a90c2012369338ac2
paulatanu1 <atanupaul76@gmail.com>
2023-06-10 00:09:59 +0530

    encrypted local storage added

--- frontend/src/app/common-service/api.service.ts
+++ frontend/src/app/common-service/api.service.ts
@@ -1,93 +1,143 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map, timeout, catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs'
 import { environment } from '../../environments/environment';
+import { map, timeout, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { Router } from '@angular/router';
+import ls from 'localstorage-slim';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+ls.config.encrypt = environment.LS_CONFIG_ENCRYPT;
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+  ) {}
 
-  constructor(private http: HttpClient,) { }
-
-  queryParams(source: any) {
-    var array = [];
-    for (var key in source) {
-      array.push(encodeURIComponent(key) + "=" + encodeURIComponent(source[key]));
-    }
-    return array.join("&");
-  }
-
-  /**
-    @Date : 23-05-2023
-    @Developer : Atanu Paul
-    @param url : api url to call like '/regisatration ,/login'
-    @param method : method to call like post or put or get or delete
-   */
-  ApiCallWithLocalization(data: any, url: string, method: string, headertoken: any = undefined) {
+  ApiCallWithLocalization(
+    data: any,
+    url: any,
+    method: any,
+    headertoken: any = undefined
+  ) {
+    // let lang = this._commonHelperService.GetLanguage();
     return this.ApiCall(data, url, method, headertoken);
   }
 
-  ApiCall(data: any, url: string, method: string, headertoken: any = undefined, IsUrlrawformat: boolean = false) {
+  ApiCall(
+    data: any,
+    url: any,
+    method: any,
+    headertoken: any = undefined,
+    localization: any = 'en',
+    IsUrlrawformat: boolean = false
+  ) {
     let paths: string[] = location.pathname.split('/').splice(1, 1);
-    
 
     if (IsUrlrawformat == false) {
       url = environment.API_URL + '/' + environment.API_VERSION + url;
     }
 
-    console.log(headertoken)
     let httpHeaderValue = new HttpHeaders();
-
     if (headertoken == undefined) {
-      console.log(httpHeaderValue)
-      httpHeaderValue = httpHeaderValue
-    }
-    else {
+      httpHeaderValue = httpHeaderValue.set('X-localization', localization);
+    } else {
       httpHeaderValue = httpHeaderValue
         .set('Authorization', 'Bearer ' + headertoken)
+        //.set('Content-Type', 'application/json')
+        .set('X-localization', localization);
     }
 
     if (method == 'post') {
-      return this.http.post(url, data, { headers: httpHeaderValue, observe: 'response' })
+      return this.http
+        .post(url, data, { headers: httpHeaderValue, observe: 'response' })
         .pipe(
-          timeout(environment.timeout),
-          catchError((e, c) => { return throwError(e) }),
-          map((response: any) => {
-            console.log(responseobj)
-            var responseobj = JSON.parse(JSON.stringify(response.body));
-            responseobj.status = response.status;
-            return responseobj;
-          })
-        )
-
-    }
-    else if (method == 'get') {
-      return this.http.get(url, { headers: httpHeaderValue, observe: 'response' })
-        .pipe(
-          timeout(environment.timeout),
-          catchError((e, c) => { return throwError(e) }),
+          timeout(environment.API_TIMEOUT),
+          catchError((e, c) => {
+            return throwError(e);
+          }),
           map((response: any) => {
             var responseobj = JSON.parse(JSON.stringify(response.body));
             responseobj.status = response.status;
+            console.log(responseobj.status ,'status')
+            console.log(responseobj.token , 'token');
+            if (responseobj.token != undefined) {
+              ls.set('login_token', responseobj.token);
+            }
             return responseobj;
           })
-        )
-
-    }
-    else {
-      return this.http.post(url, data, { headers: httpHeaderValue, observe: 'response' })
+        );
+    } else if (method == 'get') {
+      return this.http
+        .get(url, { headers: httpHeaderValue, observe: 'response' })
         .pipe(
-          timeout(environment.timeout),
-          catchError((e, c) => { return throwError(e) }),
+          timeout(environment.API_TIMEOUT),
+          catchError((e, c) => {
+            return throwError(e);
+          }),
           map((response: any) => {
             var responseobj = JSON.parse(JSON.stringify(response.body));
             responseobj.status = response.status;
+            if (responseobj.token != undefined) {
+              ls.set('login_token', responseobj.token);
+            }
             return responseobj;
           })
-        )
-
+        );
+    } else {
+      return this.http
+        .post(url, data, { headers: httpHeaderValue, observe: 'response' })
+        .pipe(
+          timeout(environment.API_TIMEOUT),
+          catchError((e, c) => {
+            return throwError(e);
+          }),
+          map((response: any) => {
+            if (response.status == 204) {
+              return response;
+            }
+            var responseobj = JSON.parse(JSON.stringify(response.body));
+            responseobj.status = response.status;
+            if (responseobj.token != undefined) {
+              ls.set('login_token', responseobj.token);
+            }
+            return responseobj;
+          })
+        );
     }
   }
 
+  DeleteAdminUserInfoFromLocalStorage() {
+    ls.remove('login_token');
+  } //.End of DeleteAdminUserInfoFromLocalStorage()
+
+  HandleErrorCode(err: any, callback: any = null) {
+    let StatusCode: number = err.status;
+    let ERROR_REDIRECT = environment.ERROR_REDIRECT;
+    if (err.status == 401) {
+      StatusCode = 401;
+      this.DeleteAdminUserInfoFromLocalStorage();
+      this.router.navigate(['/login']);
+    } else if (err.status == 404 && ERROR_REDIRECT == true) {
+      this.router.navigate(['/page-not-found']);
+    } else if (err.status == 500 && ERROR_REDIRECT == true) {
+      this.router.navigate(['/internal-server-error']);
+    }
+    //return StatusCode;
+  } //.End of HandleError()
+
+  HandleSuccessCode(result: any, callback: any = null) {
+    let StatusCode: number = result.status;
+    if (result.status == 202) {
+      StatusCode = 202;
+      ls.set('login_token', result.response.raws.data.token);
+    }
+    //return StatusCode;
+  } //.End of HandleSuccessCode()
+
+  GetUserInfoFromLocalStorage() {
+    return ls.get('login_token');
+  }
 }
