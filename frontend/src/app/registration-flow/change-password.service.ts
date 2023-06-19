52be200a71871a6aff5cb28ee989c036f392c5af
paulatanu1 <atanupaul76@gmail.com>
2023-06-20 00:26:31 +0530

    api service implement new

--- frontend/src/app/registration-flow/change-password.service.ts
+++ frontend/src/app/registration-flow/change-password.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../common-service/api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChangePasswordService {
 
-  constructor() { }
+  constructor(private api:ApiService) { }
+
+  resetPassword(oldPassword:string,newPassword:string){
+    let url:string = '/auth/change-password';
+
+    const form_data:any = new Object();
+    form_data.old_password = oldPassword;
+    form_data.new_password = newPassword;
+
+    return this.api.ApiCallWithLocalization(form_data,url,'post')
+  }
 }
