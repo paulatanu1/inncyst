8966271d744964b0610696074279465b96ba1bf0
paulatanu1 <atanupaul76@gmail.com>
2023-08-24 01:24:15 +0530

    add job button added in listing page

--- frontend/src/app/industry/jobs-management/jobs-management.component.ts
+++ frontend/src/app/industry/jobs-management/jobs-management.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { LeftMenuHandelService } from '../left-menu-handel.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-jobs-management',
   templateUrl: './jobs-management.component.html',
-  styleUrls: ['./jobs-management.component.scss']
+  styleUrls: ['./jobs-management.component.scss'],
 })
 export class JobsManagementComponent implements OnInit {
-
-  constructor(private _menuHandel:LeftMenuHandelService) { }
+  constructor(
+    private _menuHandel: LeftMenuHandelService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-
-    this._menuHandel.leftMenuActive.next(1)
+    this._menuHandel.leftMenuActive.next(1);
   }
 
+  addJobs() {
+    this.router.navigate(['/industry/jobs/add-job']);
+  }
 }
