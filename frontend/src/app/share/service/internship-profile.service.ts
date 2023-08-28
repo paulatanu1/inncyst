ccb3d31078b0567e8bcc163042d5bae867176e8c
paulatanu1 <atanupaul76@gmail.com>
2023-08-28 22:23:01 +0530

    my profile api

--- frontend/src/app/share/service/internship-profile.service.ts
+++ frontend/src/app/share/service/internship-profile.service.ts
@@ -2,16 +2,13 @@ import { Injectable } from '@angular/core';
 import { ApiService } from 'src/app/common-service/api.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InternshipProfileService {
-
-  constructor(private api:ApiService) {
-    
-   }
-   sendInternshipProfileRequest(){
-    let url= '/intranship/intranships';
-    const form_data:any = new Object();
-      return this.api.ApiCallWithLocalization(form_data,url,'get')
-   }
+  constructor(private api: ApiService) {}
+  sendInternshipProfileRequest() {
+    let url = '/auth/me';
+    // const form_data:any = new Object();
+    return this.api.ApiCallWithLocalization('', url, 'get');
+  }
 }
