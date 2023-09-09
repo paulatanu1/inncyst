7225e526aa648ac2951ec315eec1db22c7f4c061
paulatanu1 <atanupaul76@gmail.com>
2023-09-10 00:33:10 +0530

    edit profile in-progress

--- frontend/src/app/share/my-profile/my-profile.component.ts
+++ frontend/src/app/share/my-profile/my-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { InternshipProfileService } from '../service/internship-profile.service';
 import { Subscription } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 interface IprofileDetails {
   name: string;
@@ -12,6 +13,9 @@ interface IprofileDetails {
   question_step: boolean;
   status: boolean;
   createdAt: string;
+  location: null | string;
+  description: null | string;
+  skills: string[];
 }
 @Component({
   selector: 'app-my-profile',
@@ -21,15 +25,45 @@ interface IprofileDetails {
 export class MyProfileComponent implements OnInit {
   ProfileDetails!: IprofileDetails;
   profile: Subscription | undefined;
-  constructor(private internship: InternshipProfileService) {}
+  editProfile: boolean = false;
+  profileForm: FormGroup = new FormGroup({});
+
+  constructor(
+    private internship: InternshipProfileService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.profileForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      shortDescription: ['', Validators.required],
+      skills: [[]], // Initialize as an empty array
+      location: [''],
+      phone: ['', Validators.required],
+      email: ['', Validators.required],
+    });
+
     this.profile = this.internship
       .sendInternshipProfileRequest()
       .subscribe((response) => {
         this.ProfileDetails = response.data;
         console.log(this.ProfileDetails);
       });
+  }
+
+  openEdit() {
+    this.editProfile = true;
+  }
+
+  onSubmit() {
+    if (this.profileForm.valid) {
+      // Form is valid, you can access form values using this.profileForm.value
+      console.log(this.profileForm.value);
+
+      // You can send the form data to your backend or perform other actions here
+    } else {
+      // Form is invalid, display error messages or perform other actions as needed
+    }
   }
 
   ngOnDestroy() {
