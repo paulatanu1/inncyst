e445e9bb7ab07865b139f711dbcaaedb16208e44
rupam090820 <rupams441@gmail.com>
2023-09-12 00:32:17 +0530

    api implementaion (getAll job, upload resume,upload resume  submit), upload resume base64  to multipart for_data convertion

--- frontend/src/app/service/jobs.service.ts
+++ frontend/src/app/service/jobs.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '../common-service/api.service';
+import ls from 'localstorage-slim';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JobsService {
 
-  constructor(private api: ApiService) { }
-  
-  getAllJobDetails(){
-    let url:string='/job/jobs'
+  constructor(private api: ApiService) { 
+  }
+  getAllJobDetails(detail:any,url:any){
     return this.api.ApiCallWithLocalization('', url, 'get');
   }
 
+  uploadResume(data:any){
+    let url='/student/upload-resume'
+    const form_data = new FormData();
+    form_data.append('jobId',data.jobId)
+    form_data.append('resume',data.resume)
+    return this.api.ApiCallWithLocalization(form_data,url,'post')
+  }
   applyJob(details:any){
     let url= '/student/apply-job';
     return this.api.ApiCallWithLocalization(details,url,'post')
