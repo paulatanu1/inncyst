dc3b36f6e6e50128d943abb9c581511c8575da60
paulatanu1 <atanupaul76@gmail.com>
2023-11-03 00:27:25 +0530

    youtube play issue fixed

--- frontend/src/app/pipe/sanitizer-url.pipe.ts
+++ frontend/src/app/pipe/sanitizer-url.pipe.ts
@@ -2,14 +2,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Pipe({
-  name: 'sanitizerUrl'
+  name: 'sanitizerUrl',
 })
 export class SanitizerUrlPipe implements PipeTransform {
-
-  constructor(private _sanitizer: DomSanitizer) { }
+  constructor(private _sanitizer: DomSanitizer) {}
 
   transform(value: string): SafeHtml {
-    return this._sanitizer.bypassSecurityTrustHtml(value);
+    return this._sanitizer.bypassSecurityTrustResourceUrl(value);
   }
-
 }
