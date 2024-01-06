94c81b6aec01e552fa02ff0dc750ea10fd845344
MANAS PRAMANIK <manas@pitangent.com>
2024-01-06 08:43:28 +0530

    add production path

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   timeout: 2500,
-  API_URL: 'http://localhost:5500',
+  API_URL: 'http://13.233.255.76:5500',
   API_VERSION: 'api',
   LS_CONFIG_ENCRYPT: true,
   MSG_TIMEOUT: 2000,
