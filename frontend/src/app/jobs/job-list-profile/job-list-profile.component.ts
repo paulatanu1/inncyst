f015ff3a2bf70a2125037fa43a0b799f80f0ef4d
Sohini Sengupta <sohini.sengupta@massoftind.com>
2024-02-15 02:32:34 +0530

    Job left panel design

---
+++ frontend/src/app/jobs/job-list-profile/job-list-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-job-list-profile',
+  templateUrl: './job-list-profile.component.html',
+  styleUrls: ['./job-list-profile.component.scss']
+})
+export class JobListProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
