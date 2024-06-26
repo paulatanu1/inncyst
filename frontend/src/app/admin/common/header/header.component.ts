02fa22da6518720c987697b82e4010e2aa22979d
paulatanu1 <atanupaul76@gmail.com>
2024-06-27 00:58:50 +0530

    admin dashboard design complete

--- frontend/src/app/admin/common/header/header.component.ts
+++ frontend/src/app/admin/common/header/header.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  currentTheme: string | null = '';
+  userRole!: string;
+  constructor(private renderer: Renderer2) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  switchTheme(event: any) {
+    if (event.target.checked) {
+      this.enableDarkTheme();
+      localStorage.setItem('theme', 'dark');
+    } else {
+      this.enableLightTheme();
+      localStorage.setItem('theme', 'light');
+    }
   }
 
+  enableDarkTheme() {
+    this.renderer.addClass(document.body, 'dark-mode');
+    const mainHeader = document.querySelector('.main-header');
+    if (mainHeader) {
+      this.renderer.addClass(mainHeader, 'navbar-dark');
+      this.renderer.removeClass(mainHeader, 'navbar-light');
+    }
+  }
+
+  enableLightTheme() {
+    this.renderer.removeClass(document.body, 'dark-mode');
+    const mainHeader = document.querySelector('.main-header');
+    if (mainHeader) {
+      this.renderer.addClass(mainHeader, 'navbar-light');
+      this.renderer.removeClass(mainHeader, 'navbar-dark');
+    }
+  }
 }
